55b9bf816e098d35e705f346663974647d1c133e
kilobyte97 <balakshm@microsoft.com>
2021-07-09 13:49:23 +0530

    resources for pipeline

--- ARMDeploymentTemplate/deploymentscript.ps1
+++ ARMDeploymentTemplate/deploymentscript.ps1
@@ -1,12 +1,14 @@
 param(
             [string] [Parameter(Mandatory=$true)] $clusterUrl,
             [string] [Parameter(Mandatory=$true)] $dbName,
-            [string] [Parameter(Mandatory=$true)] $tableName,
+            [string] [Parameter(Mandatory=$true)] $logsTableName,
+            [string] [Parameter(Mandatory=$true)] $metricsTableName,
 			[string] [Parameter(Mandatory=$true)] $adfIdentity
           )
 Write-Host $clusterUrl
 Write-Host $dbName
-Write-Host $tableName
+Write-Host $logsTableName
+Write-Host $metricsTableName
 Write-Host $adfIdentity
 
 
@@ -28,7 +30,14 @@ $result | ConvertTo-Json -Depth 5
 
 $body = @{
  "db"="$dbName"
- "csl"=".create table [$tableName]  ([defectid]:string,[filepath]:string,[_index]:string,[_type]:string,[_id]:string,[_source] : dynamic,[_source_stream] : string,[_source_docker_container_id]: string,[_source_kubernetes_container_name] : string,[_source_kubernetes_namespace_name] : string,[_source_kubernetes_pod_name] : string,[_source_kubernetes_container_image] : string,[_source_kubernetes_host] : string,[_source_time]: datetime ,[_source_pei] : string,[_source_supi] : string,[_source_tid] : string,[_source_pduSessionid] : string,[_source_pcfid] : string,[_source_method] : string,[_source_eventLogger] : string,[_source_debug_string] : string,[_source_severity] : string)"
+ "csl"=".create table [$logsTableName]  ([defectid]:string,[filepath]:string,[_index]:string,[_type]:string,[_id]:string,[_source] : dynamic,[_source_stream] : string,[_source_docker_container_id]: string,[_source_kubernetes_container_name] : string,[_source_kubernetes_namespace_name] : string,[_source_kubernetes_pod_name] : string,[_source_kubernetes_container_image] : string,[_source_kubernetes_host] : string,[_source_time]: datetime ,[_source_pei] : string,[_source_supi] : string,[_source_tid] : string,[_source_pduSessionid] : string,[_source_pcfid] : string,[_source_method] : string,[_source_eventLogger] : string,[_source_debug_string] : string,[_source_severity] : string)"
+} | ConvertTo-Json
+$result = Invoke-RestMethod -Uri "$clusterUrl/v1/rest/mgmt" -Method 'Post' -Body $body -Headers $header 
+
+
+$body = @{
+ "db"="$dbName"
+ "csl"=".create table [$metricsTableName]  ([defectid]:string,[filepath]:string,[_index]:string,[_type]:string,[_id]:string,[_source] : dynamic,[_source_stream] : string,[_source_docker_container_id]: string,[_source_kubernetes_container_name] : string,[_source_kubernetes_namespace_name] : string,[_source_kubernetes_pod_name] : string,[_source_kubernetes_container_image] : string,[_source_kubernetes_host] : string,[_source_time]: datetime ,[_source_pei] : string,[_source_supi] : string,[_source_tid] : string,[_source_pduSessionid] : string,[_source_pcfid] : string,[_source_method] : string,[_source_eventLogger] : string,[_source_debug_string] : string,[_source_severity] : string)"
 } | ConvertTo-Json
 $result = Invoke-RestMethod -Uri "$clusterUrl/v1/rest/mgmt" -Method 'Post' -Body $body -Headers $header 
 
--- ARMDeploymentTemplate/starttrigger.ps1
+++ ARMDeploymentTemplate/starttrigger.ps1
@@ -1,15 +1,16 @@
 param(
-            [string] [Parameter(Mandatory=$true)] $subscriptionId,
-            [string] [Parameter(Mandatory=$true)] $resourceGroup,
-            [string] [Parameter(Mandatory=$true)] $dataFactoryName,
-			[string] [Parameter(Mandatory=$true)] $triggerName
-          )
+    [string] [Parameter(Mandatory=$true)] $subscriptionId,
+    [string] [Parameter(Mandatory=$true)] $resourceGroup,
+    [string] [Parameter(Mandatory=$true)] $dataFactoryName,
+    [string] [Parameter(Mandatory=$true)] $logsTriggerName,
+    [string] [Parameter(Mandatory=$true)] $metricsTriggerName
+)
 
 Write-Host $subscriptionId
 Write-Host $resourceGroup
 Write-Host $dataFactoryName
-Write-Host $triggerName
-
+Write-Host $logsTriggerName
+Write-Host $metricsTriggerName
 
 $token=(Get-AzAccessToken).Token
 
@@ -18,10 +19,16 @@ $header = @{
  "Content-Type"="application/json"
 } 
 
-$uri="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.DataFactory/factories/$dataFactoryName/triggers/$triggerName/start?api-version=2018-06-01"
+$uri="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.DataFactory/factories/$dataFactoryName/triggers/$logsTriggerName/start?api-version=2018-06-01"
 
 $result = Invoke-RestMethod -Uri $uri -Method 'Post' -Headers $header 
 
-$uri="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.DataFactory/factories/$dataFactoryName/triggers/$triggerName/?api-version=2018-06-01"
+$uri="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.DataFactory/factories/$dataFactoryName/triggers/$logsTriggerName/?api-version=2018-06-01"
 Invoke-RestMethod -Uri $uri -Method 'Get' -Headers $header | ConvertTo-Json -Depth 5
 
+$uri="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.DataFactory/factories/$dataFactoryName/triggers/$metricsTriggerName/start?api-version=2018-06-01"
+
+$result = Invoke-RestMethod -Uri $uri -Method 'Post' -Headers $header 
+
+$uri="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.DataFactory/factories/$dataFactoryName/triggers/$metricsTriggerName/?api-version=2018-06-01"
+Invoke-RestMethod -Uri $uri -Method 'Get' -Headers $header | ConvertTo-Json -Depth 5
