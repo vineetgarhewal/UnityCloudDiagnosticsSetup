4e26d5b7a4c6dfcb40492762d1e860de613d256d
kilobyte97 <balakshm@microsoft.com>
2021-07-09 17:00:41 +0530

    table schema for metrics

--- ARMDeploymentTemplate/deploymentscript.ps1
+++ ARMDeploymentTemplate/deploymentscript.ps1
@@ -29,7 +29,7 @@ $result = Invoke-RestMethod -Uri "$clusterUrl/v1/rest/mgmt" -Method 'Post' -Body
 
 $body = @{
  "db"="$dbName"
- "csl"=".create table [$metricsTableName]  ([defectid]:string,[filepath]:string,[_index]:string,[_type]:string,[_id]:string,[_source] : dynamic,[_source_stream] : string,[_source_docker_container_id]: string,[_source_kubernetes_container_name] : string,[_source_kubernetes_namespace_name] : string,[_source_kubernetes_pod_name] : string,[_source_kubernetes_container_image] : string,[_source_kubernetes_host] : string,[_source_time]: datetime ,[_source_pei] : string,[_source_supi] : string,[_source_tid] : string,[_source_pduSessionid] : string,[_source_pcfid] : string,[_source_method] : string,[_source_eventLogger] : string,[_source_debug_string] : string,[_source_severity] : string)"
+ "csl"=".create table [$metricsTableName]  ([defectid]:string,[filepath]:string,[metricname]:string,[job]:string,[namespace]:string,[pod]:string,[quantile]:string,[state]:string,[type]:string,[event]:string,[database]:string,[values]:dynamic)"
 } | ConvertTo-Json
 $result = Invoke-RestMethod -Uri "$clusterUrl/v1/rest/mgmt" -Method 'Post' -Body $body -Headers $header 
 
